8637ea8237e2ee3e683f2148502b19034fea0a50
simsalabim08 <westra77@gmail.com>
2020-08-20 23:18:58 +0200

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       flash[:succes] = "Welcome to the Sample app!"
       redirect_to @user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,11 +8,17 @@ module SessionsHelper
   # Returns the current logged-in user (if any).
   def current_user
     if session[:user_id]
-    @current_user ||= User.find(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
   def logged_in?
     !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
+    reset_session
+    @current_user = nil
   end
 end
